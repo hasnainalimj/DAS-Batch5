c02005212162a834d9cec53987ab437ad7898686
hasnainalimj <mjhasnain15@gmail.com>
2021-10-30 20:42:51 +0500

    Class 12 Chapters#45-47

---
+++ Module 1/Class 12/js/app.js
@@ -0,0 +1,23 @@
+//Chapter # 45
+
+function sayBye() {
+  console.log('Bye bye')
+}
+
+// sayBye()
+
+//Chapter # 46
+
+function letsCheers() {
+  console.log('Cheers :)')
+}
+
+//Chapter # 47
+
+function inside() {
+  console.log('Inside')
+}
+
+function outside() {
+  console.log('Outside')
+}
